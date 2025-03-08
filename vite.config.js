e3b857dac6a4a5a96ab5bf0fdf8ee2e6f173c442
Aaryashkc <aaryashkc123@gmail.com>
2025-03-08 18:55:32 +0545

    second push

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: process.env.PORT || 5173, // Use Render's port or fallback to 5173 for local dev
+    host: '0.0.0.0', // Ensure Render can access the app
+  }
 })
